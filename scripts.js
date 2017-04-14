0251b3767fdc1caa8836c624d424d9d48b933f01
Ariana Escobar <ariana@cartodb.com>
2017-04-14 18:16:08 +0200

    test 11

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 $(function() {
    $(window).scroll(function () {
-      if ($(this).scrollTop() > 2300) {
+      if ($(this).scrollTop() > 2100) {
          $('body').addClass('changeColor3')
       }
-      if ($(this).scrollTop() < 2300) {
+      if ($(this).scrollTop() < 2100) {
          $('body').removeClass('changeColor3')
       }
-      if ($(this).scrollTop() > 1500) {
+      if ($(this).scrollTop() > 1300) {
          $('body').addClass('changeColor2')
       }
-      if ($(this).scrollTop() < 1500) {
+      if ($(this).scrollTop() < 1300) {
          $('body').removeClass('changeColor2')
       }
-      if ($(this).scrollTop() > 500) {
+      if ($(this).scrollTop() > 400) {
          $('body').addClass('changeColor')
          }
-      if ($(this).scrollTop() < 500) {
+      if ($(this).scrollTop() < 400) {
          $('body').removeClass('changeColor')
        }
    });
