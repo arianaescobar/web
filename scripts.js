8bf489d925717232c4fd5853ae73fe83d97e3972
Ariana Escobar <ariana@cartodb.com>
2017-06-26 13:47:20 +0200

    happy pride!

--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
 $(function() {
    $(window).scroll(function () {
       if ($(this).scrollTop() > 2500) {
-         $('body').addClass('changeColor3')
+         $('body').addClass('changeColor5')
       }
       if ($(this).scrollTop() < 2500) {
+         $('body').removeClass('changeColor5')
+      }
+      if ($(this).scrollTop() > 2000) {
+         $('body').addClass('changeColor4')
+      }
+      if ($(this).scrollTop() < 2000) {
+         $('body').removeClass('changeColor4')
+      }
+      if ($(this).scrollTop() > 1500) {
+         $('body').addClass('changeColor3')
+      }
+      if ($(this).scrollTop() < 1500) {
          $('body').removeClass('changeColor3')
       }
-      if ($(this).scrollTop() > 1600) {
+      if ($(this).scrollTop() > 1000) {
          $('body').addClass('changeColor2')
       }
-      if ($(this).scrollTop() < 1600) {
+      if ($(this).scrollTop() < 1000) {
          $('body').removeClass('changeColor2')
       }
-      if ($(this).scrollTop() > 400) {
+      if ($(this).scrollTop() > 500) {
          $('body').addClass('changeColor')
          }
-      if ($(this).scrollTop() < 400) {
+      if ($(this).scrollTop() < 500) {
          $('body').removeClass('changeColor')
        }
    });
