69771028779c7369684aa79143da94a9b2fbb4c4
Ariana Escobar <ariana@cartodb.com>
2017-07-14 23:36:18 +0200

    color reset

--- scripts.js
+++ scripts.js
@@ -1,27 +1,15 @@
 $(function() {
    $(window).scroll(function () {
-      if ($(this).scrollTop() > 2500) {
-         $('body').addClass('changeColor5')
-      }
-      if ($(this).scrollTop() < 2500) {
-         $('body').removeClass('changeColor5')
-      }
-      if ($(this).scrollTop() > 2000) {
-         $('body').addClass('changeColor4')
-      }
-      if ($(this).scrollTop() < 2000) {
-         $('body').removeClass('changeColor4')
-      }
-      if ($(this).scrollTop() > 1500) {
+      if ($(this).scrollTop() > 2250) {
          $('body').addClass('changeColor3')
       }
-      if ($(this).scrollTop() < 1500) {
+      if ($(this).scrollTop() < 2250) {
          $('body').removeClass('changeColor3')
       }
-      if ($(this).scrollTop() > 1000) {
+      if ($(this).scrollTop() > 1250) {
          $('body').addClass('changeColor2')
       }
-      if ($(this).scrollTop() < 1000) {
+      if ($(this).scrollTop() < 1250) {
          $('body').removeClass('changeColor2')
       }
       if ($(this).scrollTop() > 500) {
