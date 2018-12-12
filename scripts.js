fadfa577f1048114386bba49453d37b6cadb5896
Ariana Escobar <aescoba@gmail.com>
2018-12-13 00:29:03 +0100

    Taking down Halloween decorations

--- scripts.js
+++ scripts.js
@@ -1,16 +1,28 @@
 $(function() {
    $(window).scroll(function () {
-      if ($(this).scrollTop() > 3000) {
-         $('body').addClass('halloween-3')
+      if ($(this).scrollTop() > 2500) {
+         $('body').addClass('changeColor4')
       }
-      if ($(this).scrollTop() < 3000) {
-         $('body').removeClass('halloween-2')
+      if ($(this).scrollTop() < 2500) {
+         $('body').removeClass('changeColor4')
       }
-      if ($(this).scrollTop() > 1000) {
-         $('body').addClass('halloween-2')
+      if ($(this).scrollTop() > 1800) {
+         $('body').addClass('changeColor3')
+      }
+      if ($(this).scrollTop() < 1800) {
+         $('body').removeClass('changeColor3')
+      }
+      if ($(this).scrollTop() > 1200) {
+         $('body').addClass('changeColor2')
+      }
+      if ($(this).scrollTop() < 1200) {
+         $('body').removeClass('changeColor2')
+      }
+      if ($(this).scrollTop() > 600) {
+         $('body').addClass('changeColor')
          }
-      if ($(this).scrollTop() < 1000) {
-         $('body').removeClass('body-halloween')
+      if ($(this).scrollTop() < 600) {
+         $('body').removeClass('changeColor')
        }
    });
 });
